f53af7f851eea67f3b93ea7f67341e5617e1bc05
unknown <tampambro999@gmail.com>
2024-11-10 22:08:28 +0300

    esLint fix

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -16,7 +16,14 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+        "endOfLine": "auto",
+        "arrowParens": "avoid"
+			},
+		],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
